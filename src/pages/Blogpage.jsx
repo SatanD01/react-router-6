b32ee2eec168d775f37b5d1b664b6f357473e598
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-09-29 11:43:55 +0500

    useSearchparams, use get parametors, create posts filter by text and checkbox, save params in form

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,8 +1,17 @@
 import {useState, useEffect} from "react";
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
+import BlogFilter from "../components/BlogFilter";
 
 const Blogpage = () => {
     const [posts, setPosts] = useState();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const postQuery = searchParams.get('post') || '';
+    const latest = searchParams.has('latest');
+
+    const startsFrom = latest ? 80  : 1;
+
+
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -13,9 +22,13 @@ const Blogpage = () => {
     return  (
         <>
             <h1>Blog page</h1>
+            <BlogFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams} />
+
             <Link to='/blog/new'>Add new Post</Link>
             {
-                posts?.map(post => (
+                posts?.filter(
+                    post => post.title.includes(postQuery) && post.id >= startsFrom
+                ).map(post => (
                     <Link key={post.id} to={`/blog/${post.id}`}>
                         <li>{[post.title]}</li>
                     </Link>
